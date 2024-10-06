857046fc029e5d5a2715a31ad61f29a87d48c71b
stk346 <stk346@gmail.com>
2024-10-07 01:07:42 +0900

    feat: 영화 상세에서 별점 기능 구현

--- src/app/movie/StarInput.tsx
+++ src/app/movie/StarInput.tsx
@@ -1,43 +1,61 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 import { FaStar, FaStarHalf } from "react-icons/fa";
 
+// shouldForwardProp을 적용하여 커스텀 속성이 전달되지 않도록 설정
+const Label = styled('label', {
+  shouldForwardProp: (prop) => prop !== 'isHalf'
+})`
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: lightgray;
+
+  ${({ isHalf }) =>
+    isHalf &&
+    css`
+      position: absolute;
+      width: 12px;
+      overflow: hidden;
+
+      &:nth-of-type(10) {
+        transform: translate(-108px);
+      }
+      &:nth-of-type(8) {
+        transform: translate(-84px);
+      }
+      &:nth-of-type(6) {
+        transform: translate(-60px);
+      }
+      &:nth-of-type(4) {
+        transform: translate(-36px);
+      }
+      &:nth-of-type(2) {
+        transform: translate(-12px);
+      }
+    `}
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
 const StarInput = ({ onClickRating, value, isHalf }) => {
-    const handleClickRatingInput = () => {
-        onClickRating(value);
-    };
+  const handleClickRatingInput = () => {
+    onClickRating(value);
+  };
 
-    // 위치를 자동으로 조정하는 스타일
-    const getStarPosition = (value) => {
-        const starWidth = 24;
-        return {
-            transform: isHalf ? `translateX(-${starWidth / 2}px)` : undefined
-        };
-    };
-
-    return (
-        <>
-            <input type="radio" name="rating" id={`star${value}`} value={value} className="hidden" />
-            <label onClick={handleClickRatingInput} htmlFor={`star${value}`}
-                className={`cursor-pointer flex items text-2xl center text-gray-400
-                            ${isHalf ? 'overflow-hidden text-red-400' : ''}`}
-            >
-                
-                {isHalf
-                    ? <FaStarHalf className="absolute transform -translate-y-3 overflow-hidden"/>
-                    : <FaStar/>}
-                {/*
-                {isHalf ? (
-                    <>
-                        // 빈 별이 별과 겹치도록 설정
-                        <FaStarHalf className="absolute text-red-400 z-10 transform translate-y-3" />
-                    </>
-                ) : (
-                    <FaStar className="z-0" />
-                )}
-                */}
-
-            </label>
-        </>
-    );
-}
+  return (
+    <>
+      <Input type="radio" name="rating" id={`star${value}`} value={value} />
+      <Label
+        onClick={handleClickRatingInput}
+        isHalf={isHalf}
+        htmlFor={`star${value}`}
+      >
+        {isHalf ? <FaStarHalf /> : <FaStar />}
+      </Label>
+    </>
+  );
+};
 
 export default StarInput;
--- src/app/movie/StarRating.tsx
+++ src/app/movie/StarRating.tsx
@@ -1,46 +1,104 @@
+import styled from "@emotion/styled";
 import { useState } from "react"
 import StarInput from "./StarInput";
 
+const Base = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Name = styled.span`
+  font-size: 1.4rem;
+  line-height: 100%;
+`;
+
+const RatingValue = styled.span`
+  font-size: 1.2rem;
+  line-height: 100%;
+`;
+
+const RatingField = styled.fieldset`
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
+  border: none;
+  transform: translateY(2px);
+
+  input:checked ~ label,
+  labeL:hover,
+  labeL:hover ~ label {
+    transition: 0.2s;
+    color: orange;
+  }
+`;
+
 const StarRating = () => {
-    const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(0);
 
-    const handleClickRating = (value) => {
-        setRating(value);
-    }
+  const handleClickRating = (value) => {
+    setRating(value);
+  };
 
-    return (
-
-        <section className="flex items-center gap-2">
-            <span className="text-lg">별점</span>
-            <fieldset className="relative flex items-center">
-                {[0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5].map((value) => (
-                    <StarInput
-                        key={value}
-                        onClickRating={handleClickRating}
-                        value={value}
-                        isHalf={value % 1 !== 0} // 0.5 => isHalf=true
-                    />
-                ))}
-                {/* <StarInput onClickRating={handleClickRating} value={5} isHalf={false} />
-                <StarInput onClickRating={handleClickRating} value={4.5} isHalf={true} />
-                <StarInput onClickRating={handleClickRating} value={4} isHalf={false} />
-                <StarInput onClickRating={handleClickRating} value={3.5} isHalf={true} />
-                <StarInput onClickRating={handleClickRating} value={3} isHalf={false} />
-                <StarInput onClickRating={handleClickRating} value={2.5} isHalf={true} />
-                <StarInput onClickRating={handleClickRating} value={2} isHalf={false} />
-                <StarInput onClickRating={handleClickRating} value={1.5} isHalf={true} />
-                <StarInput onClickRating={handleClickRating} value={1} isHalf={false} />
-                <StarInput onClickRating={handleClickRating} value={0.5} isHalf={true} /> */}
-            </fieldset>
-            <span className="text-base">{rating}</span>
-        </section>
-
-
-        // <div>
-        //     <HandleRatingStars rating={rating} setRating={setRating} />
-        //     <p className="text-black-mt-2">현재 별점: {rating} 점</p>
-        // </div>
-    )
-}
+  return (
+    <Base>
+      <Name>별점</Name>
+      <RatingField>
+        <StarInput
+          onClickRating={handleClickRating}
+          value={10}
+          isHalf={false}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={9}
+          isHalf={true}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={8}
+          isHalf={false}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={7}
+          isHalf={true}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={6}
+          isHalf={false}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={5}
+          isHalf={true}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={4}
+          isHalf={false}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={3}
+          isHalf={true}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={2}
+          isHalf={false}
+        />
+        <StarInput
+          onClickRating={handleClickRating}
+          value={1}
+          isHalf={true}
+        />
+      </RatingField>
+      <RatingValue>{rating}</RatingValue>
+    </Base>
+  );
+};
 
 export default StarRating;
